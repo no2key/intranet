9fb8f7000904d933cf7e46b92cbd6bf493e3bfa7
jackfeng <jacky.ff@gmail.com>
2013-03-25 01:06:01 +0800

    use muti-cache to avoid hitting the 1M's limit of memcache

--- mc/cache.py
+++ mc/cache.py
@@ -1,16 +1,55 @@
 # -*- coding: utf-8 -*-
 import logging
+import sys
 import datetime
+import pickle
 from google.appengine.api import memcache
 
 import models
 
+def mem_set(key, value, chunksize=900000):
+  serialized = pickle.dumps(value, 2)
+  values = {}
+  for i in xrange(0, len(serialized), chunksize):
+    values['%s.%s' % (key, i//chunksize)] = serialized[i : i+chunksize]
+    logging.info("cached: " + ('%s.%s' % (key, i//chunksize))+ str(values['%s.%s' % (key, i//chunksize)]));
+  memcache.set_multi(values)
+  memcache.set(key, len(values))
+
+def mem_get(key):
+  n = memcache.get(key)
+  logging.info("#key_size:" + str(n))
+  if not n:
+    return None
+  
+  results = memcache.get_multi(['%s.%s' % (key, i) for i in xrange(n)])
+  logging.info("#results:" + str(results))
+  serialized = ''
+  chunks = []
+  for v in results.values():
+    chunks.append(v)
+  
+  serialized = ''.join(chunks)
+  try:
+    logging.info("cached hitted !!!!")
+    return pickle.loads(serialized)
+  except Exception:
+    logging.info("cache missed ")
+    return None
+
+def mem_delete(key):
+  n = memcache.get(key)
+  if n:
+    memcache.delete_multi(['%s.%s' % (key, i) for i in xrange(n)])
+  
+  memcache.delete(key)
+
 def get(key, clear=False):
     if clear:
-        memcache.delete(key)
+        mem_delete(key)
         return
 
-    data = memcache.get(key)
+    data = mem_get(key)
     if data:
         logging.info("return " + key)
         return data
@@ -21,7 +60,8 @@ def get(key, clear=False):
     elif key == "all_projects":
         query = models.Project.all().order('order')
     elif key == "all_entries":
-        query = models.Entry.all().order('due_on')
+        query = models.Entry.gql(
+            "ORDER BY due_on DESC LIMIT 100")
     elif key == "all_active_entries":
         query = models.Entry.gql(
             "WHERE status IN ('new', 'in progress', 'ready', 'completed') "
@@ -79,6 +119,7 @@ def get(key, clear=False):
     for item in query:
       data.append(item)
 
-    memcache.set(key, data)
+    mem_set(key, data)
+	
     logging.info("cached " + key)
     return data
